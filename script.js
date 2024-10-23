c481c22bf122499097ef88e7e4e6694b94d2e2d1
gafu2317 <0002317rynkmk@gmail.com>
2024-10-23 11:22:31 +0900

    入力フォームを予約ように作り替えた

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const liffId = "2006484950-WLVJM5vB"; // LIFF IDをここに入力
+  initializeLiff(liffId);
+});
+
+function initializeLiff(liffId) {
+  liff
+    .init({
+      liffId: liffId,
+    })
+    .then(() => {
+      initializeApp();
+    })
+    .catch((err) => {
+      console.log("LIFF Initialization failed ", err);
+    });
+}
+
+function sendText(text) {
+  liff
+    .sendMessages([
+      {
+        type: "text",
+        text: text,
+      },
+    ])
+    .then(function () {
+      liff.closeWindow();
+    })
+    .catch(function (error) {
+      window.alert("Failed to send message " + error);
+    });
+}
+
+document
+  .getElementById("reservationForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // デフォルトのフォーム送信を防ぐ
+
+    const name = document.getElementById("name").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+
+    const msg = `${name}\n${date}\n: ${time}`;
+    sendText(msg);
+  });
