f7d6f8bab71428e65e3255a694300fcdb0a424c4
gafu2317 <0002317rynkmk@gmail.com>
2024-10-28 13:39:25 +0900

    メッセージを送る方式に変更

--- reservation/script.js
+++ reservation/script.js
@@ -17,55 +17,81 @@ function initializeLiff(liffId) {
     });
 }
 
-//データ送信
+function sendText(text) {
+  liff
+    .sendMessages([{ type: "text", text: text }])
+    .then(function () {
+      liff.closeWindow();
+    })
+    .catch(function (error) {
+      window.alert("Failed to send message " + error);
+    });
+}
+
 document
   .getElementById("reservationForm")
   .addEventListener("submit", function (event) {
-    event.preventDefault(); // デフォルトのフォーム送信を防ぐ
+    event.preventDefault(); // フォームのデフォルトの送信を防ぐ
 
-    // ユーザープロフィールを取得
-    liff
-      .getProfile()
-      .then((profile) => {
-        const sendData = {
-          nameType: document.querySelector('input[name="nameType"]:checked')
-            .value,
-          userId: profile.userId,
-          name: document.getElementById("name").value,
-          date: document.getElementById("date").value,
-          time: document.getElementById("time").value,
-        };
+    const nameType = document.querySelector('input[name="nameType"]:checked') 
+    const name = document.getElementById("name").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
 
-        //gasにデータを送信
-        fetch(
-          "https://script.google.com/macros/s/AKfycbzCKMUEE71UKxhZs2S_5_JbqxjbYAbvOIt3AxgVCsbpjahY3W8wPgdoPezP1vfx4vh17Q/exec",
-          {
-            method: "POST",
-            mode: "no-cors",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(sendData),
-          }
-        )
-          .then((response) => {
-            // レスポンスが成功した場合にのみJSONを解析
-            if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-          })
-          .then((data) => {
-            alert("予約が完了しました");
-            document.getElementById("reservationForm").reset(); // フォームをリセット
-          })
-          .catch((error) => {
-            alert("エラーが発生しました: " + error.message);
-            console.error("Error:", error);
-          });
-      })
-      .catch((err) => {
-        console.error("Error getting profile: ", err);
-      });
-
+    const msg = `${nameType}\n${name}\n${date}\n${time}`;
+    sendText(msg);
   });
+
+//javascriptから直接gasにデータを送信する場合
+// //データ送信
+// document
+//   .getElementById("reservationForm")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault(); // デフォルトのフォーム送信を防ぐ
+
+//     // ユーザープロフィールを取得
+//     liff
+//       .getProfile()
+//       .then((profile) => {
+//         const sendData = {
+//           nameType: document.querySelector('input[name="nameType"]:checked')
+//             .value,
+//           userId: profile.userId,
+//           name: document.getElementById("name").value,
+//           date: document.getElementById("date").value,
+//           time: document.getElementById("time").value,
+//         };
+
+//         //gasにデータを送信
+//         fetch(
+//           "https://script.google.com/macros/s/AKfycbzCKMUEE71UKxhZs2S_5_JbqxjbYAbvOIt3AxgVCsbpjahY3W8wPgdoPezP1vfx4vh17Q/exec",
+//           {
+//             method: "POST",
+//             mode: "no-cors",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(sendData),
+//           }
+//         )
+//           .then((response) => {
+//             // レスポンスが成功した場合にのみJSONを解析
+//             if (!response.ok) {
+//               throw new Error(`HTTP error! status: ${response.status}`);
+//             }
+//             return response.json();
+//           })
+//           .then((data) => {
+//             alert("予約が完了しました");
+//             document.getElementById("reservationForm").reset(); // フォームをリセット
+//           })
+//           .catch((error) => {
+//             alert("エラーが発生しました: " + error.message);
+//             console.error("Error:", error);
+//           });
+//       })
+//       .catch((err) => {
+//         console.error("Error getting profile: ", err);
+//       });
+
+//   });
