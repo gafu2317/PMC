63566a9a0933643ed8577e4691b14630e9902234
gafu2317 <0002317rynkmk@gmail.com>
2024-11-09 23:47:38 +0900

    fetchURL変更

--- test/script.js
+++ test/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 //テスト
 async function test() {
   const URL =
-    "https://script.google.com/macros/s/AKfycbzCKMUEE71UKxhZs2S_5_JbqxjbYAbvOIt3AxgVCsbpjahY3W8wPgdoPezP1vfx4vh17Q/exec";
+    "https://script.google.com/macros/s/AKfycbw0Vdi6AAYwQUP3BrWQEp1BjZdws31h_b3qkVXoshD6K2koZB01OucY-tx969PAfmSbdw/exec";
   try {
     const response = await fetch(URL, {
       mode: "cors",
