abe707a1b38adf377b40e7f90f8b368305f6a3dc
gafu2317 <0002317rynkmk@gmail.com>
2024-11-09 23:52:20 +0900

    alert を　logに

--- test/script.js
+++ test/script.js
@@ -9,10 +9,9 @@ async function test() {
   const URL =
     "https://script.google.com/macros/s/AKfycbw0Vdi6AAYwQUP3BrWQEp1BjZdws31h_b3qkVXoshD6K2koZB01OucY-tx969PAfmSbdw/exec";
   try {
-    const response = await fetch(URL, {
-      mode: "cors",
+    const response = await fetch(URL,{
     });
-    alert(response);
+    console.log(response);
   } catch (error) {
     alert("でエラーが発生しました: " + error);
   }
