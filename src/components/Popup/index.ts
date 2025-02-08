7c43a77d1aa06b1f0d7b4354c99c9145973b427e
gafu2317 <0002317rynkmk@gmail.com>
2025-02-08 17:52:31 +0900

    ファイル名へんこう

--- src/components/Popup/index.ts
+++ src/components/Popup/index.ts
@@ -1,6 +1,6 @@
-export {default as ReservationPopup} from './Reservation';
+export {default as ReservationPopup} from './Reserve';
 export {default as EditReservationPopup} from './Edit';
-export {default as RegistrationPopup} from './Registration';
+export {default as RegistrationPopup} from './Register';
 export {default as PresetPopup} from './Preset';
 export {default as BandPopup} from './Band';
 
