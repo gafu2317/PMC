3b3e33c62fe371daaa81bc0bcc0b037d816e340e
gafu <0002317rynkmk@gmail.com>
2025-03-19 12:58:50 +0900

    使ってないimport消した

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,7 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useLineId } from "../context/LineIdContext"; // 修正: LineIdConstext -> LineIdContext
 import { initLiff } from "../liff/liffService";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Main = () => {
   const { setLineId } = useLineId();
